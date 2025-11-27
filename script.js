2aa94029df3ea967d8f01907d802ebc810642e3d
SnehaM27 <snehamohan0227@gmail.com>
2025-11-27 18:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+
+const element = document.getElementById("level");
+let level = 0;
+let current = element;
+
+// Count parents including <html>
+while (current) {
+    level++;
+    current = current.parentElement;
+}
+
+alert("The level of the element is: " + level);
